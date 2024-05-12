baf25b62b04f2a8aa2d42261bff5be3d362a2c65
seoloo <83264303+seoloo@users.noreply.github.com>
2024-05-12 14:47:09 +0900

    Deadlock 예시

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -3,8 +3,32 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include "AccountManager.h"
+#include "UserManager.h"
+
+void Func1()
+{
+	for (int32 i = 0; i < 1; i++)
+	{
+		UserManager::Instance()->ProcessSave();
+	}
+}
+
+void Func2()
+{
+	for (int32 i = 0; i < 1; i++)
+	{
+		AccountManager::Instance()->ProcessLogin();
+	}
+}
 
 int main()
 {
+	thread t1(Func1);
+	thread t2(Func2);
 
+	t1.join();
+	t2.join();
+
+	cout << "Join Done" << endl;
 }
