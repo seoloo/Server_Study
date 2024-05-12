2c2e780e5135027065605df77b671732d5cb2754
seoloo <83264303+seoloo@users.noreply.github.com>
2024-05-12 14:11:53 +0900

    Lock() 기초

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,45 +2,63 @@
 #include "CorePch.h"
 #include <thread>
 #include <atomic>
+#include <mutex>
 
-// atomic : All or Nothing
-// -> 한번에 모두 이루어지거나 한번에 모두 이루어지지 않거나
+vector<int32> v;
 
-// DB
-// 예시) 집행검 교환
-// A라는 유저 인벤에서 집행검 뺴고
-// B라는 유저 인벤에 집행검 추가
+// Mutual Exclusive : 상호배타적
+mutex m; // 자물쇠 개념 
 
-atomic<int32> sum = 0;
-
-void Add()
+// RAII (Resource Acquisition Is Initialization)
+template<typename T>
+class LockGuard
 {
-	for (int32 i = 0; i < 100'0000; i++)
+public:
+	LockGuard(T& m)
 	{
-		sum.fetch_add(1);
+		_mutex = &m;
+		_mutex->lock();
 	}
-}
-
-void Sub()
-{
-	for (int32 i = 0; i < 100'0000; i++)
+	~LockGuard()
 	{
-		sum.fetch_sub(1);
+		_mutex->unlock();
+	}
+private:
+	T* _mutex;
+};
+
+void Push()
+{
+	for (int32 i = 0; i < 10'000; i++)
+	{
+		// 자물쇠 잠그기
+		//LockGuard<mutex> lockGuard(m); // lock_guard<mutex> lockGuard(m);
+		/*unique_lock<mutex> uniqueLock(m, defer_lock);
+
+		uniqueLock.lock();*/
+
+		// m.lock(); 
+		// m.lock(); -> 재귀적으로 호출할 수 없음
+
+		v.push_back(i);
+
+		if (i == 5000)
+			break;
+
+		// 자물쇠 풀기
+		//m.unlock(); 
+		//m.unlock(); -> 재귀적으로 호출할 수 없음
 	}
 }
 
 int main()
 {
-	Add();
-	Sub();
-
-	cout << sum << endl;
-
-	thread t1(Add);
-	thread t2(Sub);
+	v.reserve(20'000);
+	thread t1(Push);
+	thread t2(Push);
 
 	t1.join();
 	t2.join();
 
-	cout << sum << endl;
+	cout << v.size() << endl;
 }
