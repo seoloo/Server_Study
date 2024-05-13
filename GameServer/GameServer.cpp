d7d4ed327c6810e300bb35d5c590d18aeb6ea259
seoloo <83264303+seoloo@users.noreply.github.com>
2024-05-13 21:14:17 +0900

    reset

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -3,50 +3,8 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
-#include <Windows.h>
-
-mutex m;
-queue<int32> q;
-HANDLE handle;
-void Producer()
-{
-	while (true)
-	{
-		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
-		}
-		::SetEvent(handle);
-
-		//this_thread::sleep_for(100ms);
-	}
-}
-
-void Consumer()
-{
-	while (true)
-	{
-		::WaitForSingleObject(handle, INFINITE);
-
-		unique_lock<mutex> lock(m);
-		if(!q.empty())
-		{
-			int32 data = q.front();
-			q.pop();
-			cout << q.size() << endl;
-		}
-	}
-}
 
 int main()
 {
-	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*binitialState*/, NULL);
 
-	thread t1(Producer);
-	thread t2(Consumer);
-
-	t1.join();
-	t2.join();
-
-	::CloseHandle(handle);
 }
