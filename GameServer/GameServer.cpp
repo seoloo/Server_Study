283bf8bd17199fd9e573092278f93767413e2c69
seoloo <83264303+seoloo@users.noreply.github.com>
2024-05-13 21:13:18 +0900

    Condition Variable

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,7 +7,7 @@
 
 mutex m;
 queue<int32> q;
-HANDLE/*이벤트가 여러개 일텐데 번호를 부여(식별자)*/ handle;
+HANDLE handle;
 void Producer()
 {
 	while (true)
@@ -16,11 +16,9 @@ void Producer()
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
-
-		// 커널 오브젝트를 Signal 상태로 바꿔줘라는 뜻.
 		::SetEvent(handle);
 
-		this_thread::sleep_for(100ms);
+		//this_thread::sleep_for(100ms);
 	}
 }
 
@@ -28,30 +26,20 @@ void Consumer()
 {
 	while (true)
 	{
-		/* bManualReset가 FALSE이면 */
-		// Signal || Non-Signal을 확인해서 Sleep여부를 판단.
 		::WaitForSingleObject(handle, INFINITE);
-		// 만약 Signal이면 Non-Signal 상태로 바뀜
-		
-		/* bManualReset가 TRUE이면 */
-		// ::ResetEvent(handle); // 강제로 Signal 끄기
 
 		unique_lock<mutex> lock(m);
 		if(!q.empty())
 		{
 			int32 data = q.front();
 			q.pop();
-			cout << data << endl;
+			cout << q.size() << endl;
 		}
 	}
 }
 
 int main()
 {
-	// 커널 오브젝트
-	// Usage Count
-	// Signal(파란불) / Non-Signal(빨간불) << bool
-	// Auto / Manual << bool
 	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*bManualReset*/, FALSE/*binitialState*/, NULL);
 
 	thread t1(Producer);
